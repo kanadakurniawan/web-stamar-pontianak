b023ed3a238c1e9275fecb10aa78fed648f8b3fe
kanadakurniawan <kanada.kurniawan@gmail.com>
2023-06-03 19:21:14 +0700

    memindahkan icon

--- resources/views/elements/widgets/prakiraan-cuaca-pelabuhan.blade.php
+++ resources/views/elements/widgets/prakiraan-cuaca-pelabuhan.blade.php
@@ -1,9 +1,9 @@
 <div class="carousel team-members team-members-shadow" data-dots="false" data-items="3">
     @for ($i=0; $i < 20; $i++) 
-    <div class="team-member pt-4 pb-3 carouselcuaca">
+    <div class="team-member pt-4 pb-3 carouselcuaca" style="background-image: url('{{ asset('frontend/images/widgets/prakiraan-cuaca-pelabuhan/background.jpg') }}')">
         <div class="team-image mb-0 mt-2">
             <div class="icon mb-0">
-                <img alt="" src="{{ asset('frontend/images/icon/cuaca') }}/{{ $dataPelabuhan[$i][0]['weather'] }}.png" class="iconawan">									
+                <img alt="" src="{{ asset('frontend/images/widgets/prakiraan-cuaca-pelabuhan/icon') }}/{{ $dataPelabuhan[$i][0]['weather'] }}.png" class="iconawan">									
             </div>
                                
         </div> 
